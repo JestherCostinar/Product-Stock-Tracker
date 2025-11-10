7ef152dc6f41cf99dca371a3cf7f3f0b79aa6e36
Jesther Costinar <jesther.jc15@gmail.com>
2025-11-10 15:19:12 +0800

    first commit

---
+++ src/main/java/com/costinarj/ecommerce/ProductStockTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.costinarj.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductStockTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductStockTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/costinarj/ecommerce/ProductStockTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.costinarj.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductStockTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
